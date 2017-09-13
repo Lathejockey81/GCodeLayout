1cb4cdad3fc407073a342a9a9c544286faef9ce5
Lathejockey81 <n8.price@gmail.com>
2017-09-13 16:51:16 -0500

    First commit

---
+++ GCodeLayout/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GCodeLayout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<ProgramLine> CompleteProgram;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = true;
+            ofd.Filter = "NC Programs (*.NC)|*.NC|All Files (*.*)|*.*";
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == true)
+            {
+                ParseProgram(ofd.FileName);
+                
+                if (CompleteProgram == null) return;
+
+                lvProgData.ItemsSource = CompleteProgram.Where(i => i.Comments.Length > 0);
+            }
+        }
+
+        private void ParseProgram(String ProgramPath)
+        {
+            if (!System.IO.File.Exists(ProgramPath)) return;
+
+            CompleteProgram = new ObservableCollection<ProgramLine>();
+
+            try
+            {
+                IEnumerable<String> lines = File.ReadLines(ProgramPath);
+                int lineNum = 0;
+                foreach (var s in lines)
+                {
+                    lineNum++;
+                    CompleteProgram.Add(new ProgramLine(s, lineNum));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Reading File\n" + ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Decimal shiftAmount;
+            if(!Decimal.TryParse(tbZShift.Text, out shiftAmount)) return;
+
+            foreach (var i in lvProgData.SelectedItems)
+            {
+                var line = i as ProgramLine;
+                if (line != null)
+                {
+                    line.ShiftZ(shiftAmount, cbAboveZero.IsChecked ?? false);
+                }
+            }
+
+            lvProgData.Items.Refresh();
+            //lvProgData.ItemsSource = CompleteProgram.Where(i => i.Comments.Length > 0);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(String.Join("\n", CompleteProgram.Select(p => p.LineContent)));
+        }
+    }
+}
---
+++ GCodeLayout/ProgramLine.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GCodeLayout
+{
+    class ProgramLine
+    {
+        public ProgramLine() {}
+
+        public ProgramLine(String Content, int Number)
+        {
+            LineContent = Content;
+            LineNumber = Number;
+        }
+
+        private String _lineContent;
+        public String LineContent 
+        { 
+            get
+            {
+                return _lineContent;
+            }
+            set
+            {
+                _lineContent = value;
+                ExtractComments(_lineContent);
+            }
+        }
+
+        private void ExtractComments(string value)
+        {
+            Comments = String.Empty;
+            var comments = new List<String>();
+
+            // Find T Commands
+            var r = new Regex(@"^.*(T[0-9]+).*$");
+            if (r.IsMatch(value))
+            {
+                Match m = r.Match(value);
+                comments.Add(m.Groups[1].Value.ToString());
+            }
+
+            // Find Z Positions
+            r = new Regex(@"^.*(Z[0-9\.\-]+).*$");
+            if (r.IsMatch(value))
+            {
+                Match m = r.Match(value);
+                comments.Add(m.Groups[1].Value.ToString());
+                ZDim = m.Groups[1].Value.ToString();
+            }
+
+            // Find Comments
+            r = new Regex(@"^.*(\(.+\)).*$");
+            if (r.IsMatch(value))
+            {
+                Match m = r.Match(value);
+                comments.Add(m.Groups[1].Value.ToString());
+            }
+
+            if (comments.Count > 0)
+            {
+                Comments = String.Join(" ", comments);
+            }
+        }
+
+        public int LineNumber { get; set; }
+
+        public String Comments { get; set; }
+
+        public String ToolNum { get; set; }
+
+        public String XDim { get; set; }
+
+        public String YDim { get; set; }
+
+        public String ZDim { get; set; }
+
+        public void ShiftZ(Decimal Distance, Boolean ShiftAboveZero = false)
+        {
+            if (ZDim == null) return;
+
+            var r = new Regex(@"^.*Z([0-9\.\-]+).*$");
+
+            if (r.IsMatch(_lineContent))
+            {
+                Match m = r.Match(_lineContent);
+                var dim = Convert.ToDecimal(m.Groups[1].Value);
+                if (dim <= 0 || ShiftAboveZero)
+                {
+                    dim += Distance;
+                }
+
+                _lineContent = Regex.Replace(_lineContent, @"Z[0-9\.\-]+", "Z" + dim.ToString("0.####"));
+                ExtractComments(_lineContent);
+            }            
+        }
+    }
+}
